ff4b89c7912759dd079bab4b0cf5aaee549e1e82
MACBOOK PRO 2015 <macbookpro2015@macbooks-MacBook-Pro.local>
2024-11-15 13:43:34 +0700

    first commit

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+let hello = "hanafi";
+console.log(hello);
+// Array biasa
+let pacarSaya;
+pacarSaya = ["ariel tatum", "hinata"];
+//Array Tupple
+let selingkuhanya;
+selingkuhanya = ["ariel tatum", 1, true];
+console.log({ pacarSaya });
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+let hello: string = "hanafi";
+
+console.log(hello);
+
+// Array biasa
+let pacarSaya: string[];
+pacarSaya = ["ariel tatum", "hinata"];
+
+//Array Tupple
+let selingkuhanya: [string, number, boolean];
+selingkuhanya = ["ariel tatum", 1, true];
+
+console.log({ pacarSaya });
